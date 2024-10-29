fbed8eafd1763c13ef08b7652040230f6dad65af
Luc Perkins <lucperkins@gmail.com>
2024-10-29 13:07:53 -0700

    Add README, add editorconfig file, and use nix develop in CI

---
+++ README.md
@@ -0,0 +1,7 @@
+# Determinate Systems NixOS AMIs
+
+This repo houses the build logic for [Determinate Systems][detsys]' official [Amazon Machine Images (AMIs)][ami] for [NixOS].
+
+[ami]: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/AMIs.html
+[detsys]: https://determinate.systems
+[nixos]: https://zero-to-nix.com/concepts/nixos
--- flake.nix
+++ flake.nix
@@ -1,52 +1,64 @@
 {
-  inputs.flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.5.tar.gz";
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
-  inputs.determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1.95.tar.gz";
-  inputs.fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0.1.16.tar.gz";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1.95.tar.gz";
+    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0.1.*.tar.gz";
+    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.5.tar.gz";
+    nixos-amis.url = "github:NixOS/amis";
+  };
 
-  outputs = { self, nixpkgs, determinate, fh, flake-schemas, ... }:
+  outputs = { self, ... }@inputs:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      forAllSystems = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
         inherit system;
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+        lib = inputs.nixpkgs.lib;
       });
     in
     {
-      nixosConfigurations = forAllSystems
-        ({ system, pkgs, lib, ... }: lib.nixosSystem {
-          system = system;
-          modules = [
-            "${nixpkgs}/nixos/maintainers/scripts/ec2/amazon-image.nix"
-            determinate.nixosModules.default
-            {
-              environment.systemPackages = [
-                fh.packages."${system}".default
-              ];
-            }
-            {
-                systemd.services.amazon-init.path = [
-                    "/run/wrappers"
-                    "/run/current-system/sw"
-                ];
-            }
+      nixosConfigurations = forAllSystems ({ system, pkgs, lib, ... }: lib.nixosSystem {
+        system = system;
+        modules = [
+          "${inputs.nixpkgs}/nixos/maintainers/scripts/ec2/amazon-image.nix"
+          inputs.determinate.nixosModules.default
+          {
+            environment.systemPackages = [
+              inputs.fh.packages."${system}".default
+            ];
+          }
+          {
+            systemd.services.amazon-init.path = [
+              "/run/wrappers"
+              "/run/current-system/sw"
+            ];
+          }
+        ];
+      });
+
+      diskImages = forAllSystems ({ system, ... }: {
+        aws = self.nixosConfigurations.${system}.config.system.build.amazonImage;
+      });
+
+      devShells = forAllSystems ({ pkgs, system, ... }: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.smoke-test
+            inputs.nixos-amis.packages.${system}.upload-ami
           ];
-        });
+        };
+      });
 
-      diskImages = forAllSystems
-        ({ system, ... }: {
-          aws = self.nixosConfigurations.${system}.config.system.build.amazonImage;
-        });
-
-      schemas = flake-schemas.schemas // {
+      schemas = inputs.flake-schemas.schemas // {
         diskImages = {
           version = 1;
           doc = ''
             The `diskImages` flake output contains derivations that build disk images for various execution environments.
           '';
-          inventory = flake-schemas.lib.derivationsInventory "Disk image" false;
+          inventory = inputs.flake-schemas.lib.derivationsInventory "Disk image" false;
         };
       };
     };
